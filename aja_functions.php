da19a91fe36ccc80e6ba329ab27168261b63582f
AJ Acevedo <aj@ajalabs.com>
2013-09-04 12:13:40 -0400

    stripped analytics code

--- aja_functions.php
+++ aja_functions.php
@@ -18,24 +18,11 @@ remove_action('wp_head', 'wp_generator');
 remove_action('wp_head', 'wlwmanifest_link');
 
 // Adds Google Analytics to the footer
-// Edit the _setAccount and _setDomainName
+// Insert your code between the Begins and Ends block
 add_action('wp_footer', 'add_google_analytics');
   function add_google_analytics() { ?>
-<!-- Google Analytics: -->
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-XXXXXX-X']);
-  _gaq.push(['_setDomainName', 'xxxxxx.com']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
+<!-- Google Analytics: Begins-->
+<!-- Google Analytics: Ends-->
 <?php }
 
 /* Place custom code above this line. */
