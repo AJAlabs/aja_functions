d3a36d3e713f64b37246603ee576fbeb57591166
AJ Acevedo <aj@ajalabs.com>
2013-09-04 00:03:14 -0400

    Added Google Analytics - closes #3

--- aja_functions.php
+++ aja_functions.php
@@ -17,6 +17,26 @@ remove_action('wp_head', 'wp_generator');
 // Remove Windows Live Writer meta from head
 remove_action('wp_head', 'wlwmanifest_link');
 
+// Adds Google Analytics to the footer
+// Edit the _setAccount and _setDomainName
+add_action('wp_footer', 'add_google_analytics');
+  function add_google_analytics() { ?>
+<!-- Google Analytics: -->
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-XXXXXX-X']);
+  _gaq.push(['_setDomainName', 'xxxxxx.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<?php }
 
 /* Place custom code above this line. */
 ?>
